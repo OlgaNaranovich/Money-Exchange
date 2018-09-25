c0513c452b6fbdaa91aede1f293c2fa7424ce066
OLGA NARANOVICH <naranovicholga@gmail.com>
2018-09-26 00:42:04 +0300

    Money-Exchange

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
 
-		const coins = {};
-		if (currency <= 0) {
-			return coins;
-		} else if (currency > 10000) {
-			return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-		} else {
-				const num = {"H": 50,"Q": 25,"D": 10,"N": 5,"P": 1};
-				
-				 for (let i in num) {
-					let val = Math.floor(currency / num[i]);
-					if (val > 0) {
-						coins[i] = val;
-						if (currency % num[i] != 0){
-							currency = currency - val * num[i];
-							continue;
-						} break;
-					} 
-				} return coins;
-		} 
+	const coins = {};
+	if (currency <= 0) {
+		return coins;
+	} else if (currency > 10000) {
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	} else {
+		const num = {"H": 50,"Q": 25,"D": 10,"N": 5,"P": 1};
+			
+		 	for (let i in num) {
+			let val = Math.floor(currency / num[i]);
+			if (val > 0) {
+				coins[i] = val;
+				if (currency % num[i] != 0){
+					currency = currency - val * num[i];
+					continue;
+				} break;
+			} 
+		} return coins;
+	} 
 }
